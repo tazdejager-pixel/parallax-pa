02194f0297cd968b5b465ab3c3e5b4d6b4027632
Tarryn de Jager <tazdejager@gmail.com>
2026-06-25 01:07:14 +0200

    pa: allow audio uploads (.opus voice notes) + sw v14 (sw.js)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Parallax PA - service worker (offline shell + push) */
-const CACHE = "parallax-pa-v13";
+const CACHE = "parallax-pa-v14";
 const SHELL = [
   "./", "./index.html", "./styles.css", "./app.js", "./config.js",
   "./manifest.webmanifest", "./icons/icon-192.png", "./icons/icon-512.png",
